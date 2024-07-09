991835dacb23fd7653a8079832322597dac49355
VideogameScrapbook <27289568+VideogameScrapbook@users.noreply.github.com>
2024-07-09 17:27:07 -0500

    Update xbox360-marketplace-fix.bat

--- xbox360-marketplace-fix.bat
+++ xbox360-marketplace-fix.bat
@@ -1,8 +1,5 @@
 @REM This batch script is for allowing dragging files/folders onto the file's icon to easily use them as input for the Python script.
 
-@REM Rather than using this batch file, can open python script from a file's right-click menu using Types.exe (https://ystr.github.io/types/) and action similar to following:
-@REM "C:\PortableApps\WPy\python-3.7.1.amd64\pythonw.exe" "C:\scripts\foobar2000.py" "%1"%*
-
 @REM Turn on displaying of commands.
 @ECHO OFF
 
